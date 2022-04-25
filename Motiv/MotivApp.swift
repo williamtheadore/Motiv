3c990140483a81b889ad944a861481c76efa52b1
williamlittle423 <will@themotivapp.ca>
2022-04-25 16:03:25 -0400

    Initial Commit

---
+++ Motiv/MotivApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivApp.swift
+//  Motiv
+//
+//  Created by William Little on 2022-04-25.
+//
+
+import SwiftUI
+
+@main
+struct MotivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
